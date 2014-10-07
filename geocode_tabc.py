e9cca810ff3d78d17d6fd2cf384a0abbdd58bd29
ian <ian.wells@rationalsystems.com>
2014-10-06 20:35:15 -0500

    cleaned up a bit, added sample file format

--- geocode_tabc.py
+++ geocode_tabc.py
@@ -9,8 +9,18 @@ def main():
 
 	bars = []
 
-	f = open("tabc latlng 2.csv","r+")
+	f = open("tabc latlng output.csv","r+")
 
+	""" tabc houston addy could look something like:
+	12423,"10739 NORTH FWY               ","HOUSTON             ",TX,77037
+	12426,"23501 CINCO RANCH BLVD        ","HOUSTON             ",TX,77494
+	12430,"24445 KATY FWY STE 500        ","HOUSTON             ",TX,77494
+	12432,"4500 WASHINGTON AVE STE 200   ","HOUSTON             ",TX,77007
+	12435,"12611 WOODFOREST BLVD STE F   ","HOUSTON             ",TX,77015
+	12439,"16506 FM 529 RD               ","HOUSTON             ",TX,77095
+	12440,"11130 BEECHNUT ST # A-C       ","HOUSTON             ",TX,77072
+	12446,"15301 GULF FWY                ","HOUSTON             ",TX,77034
+	"""
 
 	with open('tabc houston addy.csv', 'rb') as csvfile:
 		tabcreader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -19,7 +29,7 @@ def main():
 			try:
 				b.makeLatLng()
 			except Exception, e:
-				print("couldn't get lat lng for " + b.id)
+				print("couldn't get lat lng for " + b.id + " ,try again later.")
 			
 			bars.append(b)
 			print "added bar " + b.id
